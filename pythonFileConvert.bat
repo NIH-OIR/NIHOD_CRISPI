3bf494af7fb4309d9eb59b2b2e52b3ebe01b852f
pchiang1 <chiangpt@nih.gov>
2024-12-23 13:12:24 -0500

    Added API Pipeline files

---
+++ pythonFileConvert.bat
@@ -0,0 +1,4 @@
+jupyter nbconvert --to script ClinicalTrialsAPI.ipynb
+jupyter nbconvert --to script DatabasePipeline.ipynb
+jupyter nbconvert --to script NIDBPipeline.ipynb
+jupyter nbconvert --to script PROTRAK_API_Pipeline.ipynb
