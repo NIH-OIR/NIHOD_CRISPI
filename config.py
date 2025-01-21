75266e44d07d9a67d427d012991b33b8ed544e49
lilyfan <linpei.fan@nih.gov>
2025-01-21 12:05:51 -0500

    update conf file name

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import yaml
 
 def load_config_file():
-    conf = yaml.load(open("conf.yaml"), Loader = yaml.FullLoader)
+    conf = yaml.load(open("dashboard_conf.yaml"), Loader = yaml.FullLoader)
     return conf
