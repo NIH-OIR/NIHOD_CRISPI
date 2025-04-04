b527f289739f4afa7e5af153d7e16b1d7f329168
pchiang1 <chiangpt@nih.gov>
2025-04-04 17:04:23 -0400

    added device, drug, multi institutional, is two step data

--- nci_pins_pipeline.py
+++ nci_pins_pipeline.py
@@ -38,8 +38,11 @@ def generate_nci_pins_tables(conf_file, conn):
 
     pins_data_fields_key = ['formattedProtocolNumber', 'shortTitle', 'active', 'piFullName', 'piEmail', 'piNedId', 'accrualCeiling', 
                             'sponsorNumber', 'sponsor', 'multiInstitutional', 'nihCoordinatingSite', 'externalIrbInvolved', 'externalIrbName',
-                            'protocolPhase', 'protocolStatus', 'protocolCategory', 'interventionalModel', 'masking', 'riskLevel']
+                            'protocolPhase', 'protocolStatus', 'protocolCategory', 'interventionalModel', 'masking', 'riskLevel', 
+                            'multiInstitutional', 'twoStepEnrollment']
     protocol_details_list = list()
+    pins_drug_list = list()
+    pins_device_list = list()
     i = 0
     pins_data_fields = dict.fromkeys(pins_data_fields_key, None)
     for protocol_data in protocol_list_response.json():
@@ -48,11 +51,33 @@ def generate_nci_pins_tables(conf_file, conn):
         #     pins_data_fields[key] = protocol_data[key]
         protocol_details_list.append(protocol_data)
 
+        #getting device and drug data
+        drug_data = protocol_data['drugs']
+        device_data = protocol_data['devices']
+
+        if drug_data:
+            for i in drug_data:
+                i['protocolNumber'] = protocol_data['formattedProtocolNumber']
+                pins_drug_list.append(i)
+        if device_data:    
+            for i in device_data:
+                i['protocolNumber'] = protocol_data['formattedProtocolNumber']
+                pins_device_list.append(i)
 
     pins_protocol_table = pd.json_normalize(protocol_details_list)[pins_data_fields_key]
     pins_protocol_table.insert(0,'TimeStamp',pd.to_datetime('now').replace(microsecond=0))
     pins_protocol_table.index += 1
     pins_protocol_table.to_sql('pins_protocol_table', con=conn, if_exists='replace', index=True, index_label='id')
+
+    pins_device_table = pd.json_normalize(pins_device_list)
+    pins_device_table.insert(0,'TimeStamp',pd.to_datetime('now').replace(microsecond=0))
+    pins_device_table.index += 1
+    pins_device_table.to_sql('pins_device_table', con=conn, if_exists='replace', index=True, index_label='id')
+
+    pins_drug_table = pd.json_normalize(pins_drug_list)
+    pins_drug_table.insert(0,'TimeStamp',pd.to_datetime('now').replace(microsecond=0))
+    pins_drug_table.index += 1
+    pins_drug_table.to_sql('pins_drug_table', con=conn, if_exists='replace', index=True, index_label='id')    
 
 if __name__ == '__main__': 
     conf_file = load_config_file()
